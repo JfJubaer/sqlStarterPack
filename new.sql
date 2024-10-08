ce0c7b4741e7813d464ea105db70a9470f012c1b
JfJubaer <skk5727@gmail.com>
2024-10-09 02:56:17 +0600

    constrains

--- new.sql
+++ new.sql
@@ -1,1 +1,13 @@
+-- create DATABASE test3;
+-- create table man(
+--     userName SERIAL PRIMARY KEY,
+--     age NUMERIC not NULL,
+--     email VARCHAR not NULL,
+--     UNIQUE(age,email)
+-- );
 
+insert into
+    man (username, age, email)
+values ( 1,24, 'ass@gmail.com');
+
+SELECT * from man;
