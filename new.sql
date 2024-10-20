905db3b3fc7011b2ee35340225ac42bcea4135d2
JfJubaer <skk5727@gmail.com>
2024-10-21 00:17:22 +0600

    again started with fever

--- new.sql
+++ new.sql
@@ -6,8 +6,35 @@
 --     UNIQUE(age,email)
 -- );
 
-insert into
-    man (username, age, email)
-values ( 1,24, 'ass@gmail.com');
+-- insert into
+--     man (username, age, email)
+-- values ( 1,24, 'ass@gmail.com');
 
-SELECT * from man;
+-- -- SELECT * from man;
+-- create DATABASE user1;
+
+-- -- create DATABASE friends;
+-- create Table student ( 
+--     userName SERIAL PRIMARY KEY,
+--     age NUMERIC not NULL,
+--     email VARCHAR not NULL,
+--     UNIQUE(age,email)
+-- );
+
+-- insert into
+--     man (username, age, email)
+-- values ( 3,24, 'ass@gmail.com');
+
+-- create table newOnes (
+--        userName SERIAL PRIMARY KEY,
+--     age NUMERIC not NULL,
+--     email VARCHAR not NULL,
+--     UNIQUE(age,email)
+-- )
+
+-- insert into
+--     newOnes (username, age, email)
+-- values ( 3,24, 'ass@gmail.com');
+
+select * FROM newOnes;
+
